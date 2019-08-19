37dbd6117656397517b6465038f33ece5d0259e1
Totte Harinen <23464531+t-tte@users.noreply.github.com>
2019-08-19 14:07:11 +0200

    Add meta-learner classifier classes

--- causalml/inference/meta/slearner.py
+++ causalml/inference/meta/slearner.py
@@ -18,7 +18,6 @@ class StatsmodelsOLS(object):
 
     def __init__(self, cov_type='HC1', alpha=.05):
         """Initialize a statsmodels' OLS wrapper class object.
-
         Args:
             cov_type (str, optional): covariance estimator type.
             alpha (float, optional): the confidence level alpha.
@@ -28,7 +27,6 @@ class StatsmodelsOLS(object):
 
     def fit(self, X, y):
         """Fit OLS.
-
         Args:
             X (np.matrix): a feature matrix
             y (np.array): a label vector
@@ -45,23 +43,17 @@ class StatsmodelsOLS(object):
         return self.model.predict(X)
 
 
-class BaseSLearner(object):
-    """A parent class for S-learner classes.
-
+class BaseSRegressor(object):
+    """A parent class for S-learner regressor classes.
     An S-learner estimates treatment effects with one machine learning model.
-
     Details of S-learner are available at Kunzel et al. (2018) (https://arxiv.org/abs/1706.03461).
     """
 
-    def __init__(self, learner=None, ate_alpha=0.05, control_name=0, 
-    binary_outcome=False):
+    def __init__(self, learner=None, ate_alpha=0.05, control_name=0):
         """Initialize an S-learner.
-
         Args:
             learner (optional): a model to estimate the treatment effect
             control_name (str or int, optional): name of control group
-            binary_outcome (bool, optional): whether or not the outcome is
-            binary. If True, learner must have a predict_proba() method.
         """
         if learner:
             self.model = learner
@@ -69,7 +61,6 @@ class BaseSLearner(object):
             self.model = DummyRegressor()
         self.ate_alpha = ate_alpha
         self.control_name = control_name
-        self.binary_outcome = binary_outcome
 
     def __repr__(self):
         return '{}(model={})'.format(self.__class__.__name__,
@@ -77,7 +68,6 @@ class BaseSLearner(object):
 
     def fit(self, X, treatment, y):
         """Fit the inference model
-
         Args:
             X (np.matrix): a feature matrix
             treatment (np.array): a treatment vector
@@ -96,12 +86,10 @@ class BaseSLearner(object):
 
     def predict(self, X, treatment, y=None):
         """Predict treatment effects.
-
         Args:
             X (np.matrix): a feature matrix
             treatment (np.array): a treatment vector
             y (np.array, optional): an outcome vector
-
         Returns:
             (numpy.ndarray): Predictions of treatment effects.
         """
@@ -110,19 +98,11 @@ class BaseSLearner(object):
 
         X = np.hstack((w.reshape((-1, 1)), X))
 
-        X_control = X.copy()
-        X_control[:, 0] = 0
-        
-        X_treatment = X.copy()
-        X_treatment[:, 0] = 1
-        
-        if self.binary_outcome:
-            yhat_c = self.model.predict_proba(X_control)[:, 1]
-            yhat_t = self.model.predict_proba(X_treatment)[:, 1]
+        X[:, 0] = 0    # set the treatment column to zero (the control group)
+        yhat_c = self.model.predict(X)
 
-        else:
-            yhat_c = self.model.predict(X_control)
-            yhat_t = self.model.predict(X_treatment)
+        X[:, 0] = 1    # set the treatment column to one (the treatment group)
+        yhat_t = self.model.predict(X)
 
         if y is not None:
             logger.info('RMSE (Control): {:.6f}'.format(
@@ -142,7 +122,6 @@ class BaseSLearner(object):
 
     def fit_predict(self, X, treatment, y, return_ci=False, n_bootstraps=1000, bootstrap_size=10000, verbose=False):
         """Fit the inference model of the S learner and predict treatment effects.
-
         Args:
             X (np.matrix): a feature matrix
             treatment (np.array): a treatment vector
@@ -151,7 +130,6 @@ class BaseSLearner(object):
             n_bootstraps (int, optional): number of bootstrap iterations
             bootstrap_size (int, optional): number of samples per bootstrap
             verbose (str, optional): whether to output progress logs
-
         Returns:
             (numpy.ndarray): Predictions of treatment effects. Output dim: [n_samples, n_treatment].
                 If return_ci, returns CATE [n_samples, n_treatment], LB [n_samples, n_treatment],
@@ -170,10 +148,10 @@ class BaseSLearner(object):
                 te_bootstraps[:, i] = np.ravel(te_b)
                 if verbose:
                     now = pd.datetime.today()
-                    lapsed = (now-start).seconds / 60
-                    logger.info('{}/{} bootstraps completed. ({:.01f} min lapsed)'.format(i+1, n_bootstraps, lapsed))
+                    lapsed = (now - start).seconds / 60
+                    logger.info('{}/{} bootstraps completed. ({:.01f} min lapsed)'.format(i + 1, n_bootstraps, lapsed))
 
-            te_lower = np.percentile(te_bootstraps, (self.ate_alpha/2)*100, axis=1)
+            te_lower = np.percentile(te_bootstraps, (self.ate_alpha / 2) * 100, axis=1)
             te_upper = np.percentile(te_bootstraps, (1 - self.ate_alpha / 2) * 100, axis=1)
 
             return (te, te_lower, te_upper)
@@ -194,24 +172,21 @@ class BaseSLearner(object):
         return te_b
 
 
-class LRSLearner(BaseSLearner):
+class LRSRegressor(BaseSRegressor):
     def __init__(self, ate_alpha=.05, control_name=0):
         """Initialize an S-learner with a linear regression model.
-
         Args:
             ate_alpha (float, optional): the confidence level alpha of the ATE estimate
             control_name (str or int, optional): name of control group
         """
-        super(LRSLearner, self).__init__(StatsmodelsOLS(alpha=ate_alpha), ate_alpha, control_name)
+        super(LRSRegressor, self).__init__(StatsmodelsOLS(alpha=ate_alpha), ate_alpha, control_name)
 
     def estimate_ate(self, X, treatment, y):
         """Estimate the Average Treatment Effect (ATE).
-
         Args:
             X (np.matrix): a feature matrix
             treatment (np.array): a treatment vector
             y (np.array): an outcome vector
-
         Returns:
             The mean and confidence interval (LB, UB) of the ATE estimate.
         """
@@ -223,3 +198,131 @@ class LRSLearner(BaseSLearner):
         te_lb = self.model.conf_ints[0, 0]
         te_ub = self.model.conf_ints[0, 1]
         return te, te_lb, te_ub
+
+class BaseSClassifier(object):
+    """A parent class for S-learner classifier classes.
+    An S-learner estimates treatment effects with one machine learning model.
+    Details of S-learner are available at Kunzel et al. (2018) (https://arxiv.org/abs/1706.03461).
+    """
+
+    def __init__(self, learner=None, ate_alpha=0.05, control_name=0):
+        """Initialize an S-learner.
+        Args:
+            learner (optional): a model to estimate the treatment effect
+            control_name (str or int, optional): name of control group
+        """
+        if learner:
+            self.model = learner
+        else:
+            self.model = DummyRegressor()
+        self.ate_alpha = ate_alpha
+        self.control_name = control_name
+
+    def __repr__(self):
+        return '{}(model={})'.format(self.__class__.__name__,
+                                     self.model.__repr__())
+
+    def fit(self, X, treatment, y):
+        """Fit the inference model
+        Args:
+            X (np.matrix): a feature matrix
+            treatment (np.array): a treatment vector
+            y (np.array): an outcome vector
+        """
+        is_treatment = treatment != self.control_name
+        w = is_treatment.astype(int)
+
+        t_groups = np.unique(treatment[is_treatment])
+        self._classes = {}
+
+        # this should be updated for multi-treatment case
+        self._classes[t_groups[0]] = 0
+        X = np.hstack((w.reshape((-1, 1)), X))
+        self.model.fit(X, y)
+
+    def predict(self, X, treatment, y=None):
+        """Predict treatment effects.
+        Args:
+            X (np.matrix): a feature matrix
+            treatment (np.array): a treatment vector
+            y (np.array, optional): an outcome vector
+        Returns:
+            (numpy.ndarray): Predictions of treatment effects.
+        """
+        is_treatment = treatment != self.control_name
+        w = is_treatment.astype(int)
+
+        X = np.hstack((w.reshape((-1, 1)), X))
+
+        X[:, 0] = 0
+        yhat_c = self.model.predict_proba(X)[:, 1]
+
+        X[:, 0] = 1
+        yhat_t = self.model.predict_proba(X)[:, 1]
+
+        if y is not None:
+            logger.info('RMSE (Control): {:.6f}'.format(
+                np.sqrt(mse(y[~is_treatment], yhat_c[~is_treatment])))
+            )
+            logger.info(' MAE (Control): {:.6f}'.format(
+                mae(y[~is_treatment], yhat_c[~is_treatment]))
+            )
+            logger.info('RMSE (Treatment): {:.6f}'.format(
+                np.sqrt(mse(y[is_treatment], yhat_t[is_treatment])))
+            )
+            logger.info(' MAE (Treatment): {:.6f}'.format(
+                mae(y[is_treatment], yhat_t[is_treatment]))
+            )
+
+        return (yhat_t - yhat_c).reshape(-1, 1)
+
+    def fit_predict(self, X, treatment, y, return_ci=False, n_bootstraps=1000, bootstrap_size=10000, verbose=False):
+        """Fit the inference model of the S learner and predict treatment effects.
+        Args:
+            X (np.matrix): a feature matrix
+            treatment (np.array): a treatment vector
+            y (np.array): an outcome vector
+            return_ci (bool, optional): whether to return confidence intervals
+            n_bootstraps (int, optional): number of bootstrap iterations
+            bootstrap_size (int, optional): number of samples per bootstrap
+            verbose (str, optional): whether to output progress logs
+        Returns:
+            (numpy.ndarray): Predictions of treatment effects. Output dim: [n_samples, n_treatment].
+                If return_ci, returns CATE [n_samples, n_treatment], LB [n_samples, n_treatment],
+                UB [n_samples, n_treatment]
+        """
+        self.fit(X, treatment, y)
+        te = self.predict(X, treatment, y)
+
+        if not return_ci:
+            return te
+        else:
+            start = pd.datetime.today()
+            te_bootstraps = np.zeros(shape=(X.shape[0], n_bootstraps))
+            for i in range(n_bootstraps):
+                te_b = self.bootstrap(X, treatment, y, size=bootstrap_size)
+                te_bootstraps[:, i] = np.ravel(te_b)
+                if verbose:
+                    now = pd.datetime.today()
+                    lapsed = (now - start).seconds / 60
+                    logger.info('{}/{} bootstraps completed. ({:.01f} min lapsed)'.format(i + 1, n_bootstraps, lapsed))
+
+            te_lower = np.percentile(te_bootstraps, (self.ate_alpha / 2) * 100, axis=1)
+            te_upper = np.percentile(te_bootstraps, (1 - self.ate_alpha / 2) * 100, axis=1)
+
+            return (te, te_lower, te_upper)
+
+    def estimate_ate(self, X, treatment, y):
+        te, te_lb, te_ub = self.fit_predict(X, treatment, y, return_ci=True)
+        return te.mean(), te_lb.mean(), te_ub.mean()
+
+    def bootstrap(self, X, treatment, y, size=10000):
+        """Runs a single bootstrap. Fits on bootstrapped sample, then predicts on whole population."""
+
+        idxs = np.random.choice(np.arange(0, X.shape[0]), size=size)
+        X_b = X[idxs]
+        treatment_b = treatment[idxs]
+        y_b = y[idxs]
+        self.fit(X=X_b, treatment=treatment_b, y=y_b)
+        te_b = self.predict(X=X, treatment=treatment, y=y)
+        return te_b
--- causalml/inference/meta/tlearner.py
+++ causalml/inference/meta/tlearner.py
@@ -17,16 +17,15 @@ from xgboost import XGBRegressor
 logger = logging.getLogger('causalml')
 
 
-class BaseTLearner(object):
-    """A parent class for T-learner classes.
+class BaseTRegressor(object):
+    """A parent class for T-learner regressor classes.
 
     A T-learner estimates treatment effects with two machine learning models.
 
     Details of T-learner are available at Kunzel et al. (2018) (https://arxiv.org/abs/1706.03461).
     """
 
-    def __init__(self, learner=None, control_learner=None, treatment_learner=None, ate_alpha=.05, control_name=0,
-    binary_outcome=False):
+    def __init__(self, learner=None, control_learner=None, treatment_learner=None, ate_alpha=.05, control_name=0):
         """Initialize a T-learner.
 
         Args:
@@ -35,8 +34,6 @@ class BaseTLearner(object):
             treatment_learner (model, optional): a model to estimate treatment outcomes
             ate_alpha (float, optional): the confidence level alpha of the ATE estimate
             control_name (str or int, optional): name of control group
-            binary_outcome (bool, optional): whether or not the outcome is
-            binary 
         """
         assert (learner is not None) or ((control_learner is not None) and (treatment_learner is not None))
 
@@ -52,7 +49,6 @@ class BaseTLearner(object):
 
         self.ate_alpha = ate_alpha
         self.control_name = control_name
-        self.binary_outcome = binary_outcome
 
     def __repr__(self):
         return '{}(model_c={}, model_t={})'.format(self.__class__.__name__,
@@ -69,7 +65,6 @@ class BaseTLearner(object):
             y (np.array): an outcome vector
         """
         is_treatment = treatment != self.control_name
-        w = is_treatment.astype(int)
 
         t_groups = np.unique(treatment[is_treatment])
         self._classes = {}
@@ -92,18 +87,12 @@ class BaseTLearner(object):
 
         Returns:
             (numpy.ndarray): Predictions of treatment effects.
-        """   
-        if self.binary_outcome:
-            yhat_c = self.model_c.predict_proba(X)[:, 1]
-            yhat_t = self.model_t.predict_proba(X)[:, 1]
-        
-        else:
-            yhat_c = self.model_c.predict(X)
-            yhat_t = self.model_t.predict(X)
+        """
+        yhat_c = self.model_c.predict(X)
+        yhat_t = self.model_t.predict(X)
 
         if (y is not None) and (treatment is not None):
             is_treatment = treatment != self.control_name
-            w = is_treatment.astype(int)
             logger.info('RMSE (Control): {:.6f}'.format(np.sqrt(mse(y[~is_treatment], yhat_c[~is_treatment]))))
             logger.info(' MAE (Control): {:.6f}'.format(mae(y[~is_treatment], yhat_c[~is_treatment])))
             logger.info('RMSE (Treatment): {:.6f}'.format(np.sqrt(mse(y[is_treatment], yhat_t[is_treatment]))))
@@ -141,8 +130,8 @@ class BaseTLearner(object):
                 te_bootstraps[:, i] = np.ravel(te_b)
                 if verbose:
                     now = pd.datetime.today()
-                    lapsed = (now-start).seconds / 60
-                    logger.info('{}/{} bootstraps completed. ({:.01f} min lapsed)'.format(i+1, n_bootstraps, lapsed))
+                    lapsed = (now - start).seconds / 60
+                    logger.info('{}/{} bootstraps completed. ({:.01f} min lapsed)'.format(i + 1, n_bootstraps, lapsed))
 
             te_lower = np.percentile(te_bootstraps, (self.ate_alpha / 2) * 100, axis=1)
             te_upper = np.percentile(te_bootstraps, (1 - self.ate_alpha / 2) * 100, axis=1)
@@ -169,15 +158,15 @@ class BaseTLearner(object):
         yhat_t = self.model_t.predict(X)
 
         te = (yhat_t - yhat_c).mean()
-        prob_treatment = float(sum(w))/X.shape[0]
+        prob_treatment = float(sum(w)) / X.shape[0]
 
         se = np.sqrt((
-                (y[~is_treatment] - yhat_c[~is_treatment]).var()
-                / (1 - prob_treatment) +
-                (y[is_treatment] - yhat_t[is_treatment]).var()
-                / prob_treatment +
-                (yhat_t - yhat_c).var()
-            ) / y.shape[0])
+            (y[~is_treatment] - yhat_c[~is_treatment]).var()
+            / (1 - prob_treatment) +
+            (y[is_treatment] - yhat_t[is_treatment]).var()
+            / prob_treatment +
+            (yhat_t - yhat_c).var()
+        ) / y.shape[0])
 
         te_lb = te - se * norm.ppf(1 - self.ate_alpha / 2)
         te_ub = te + se * norm.ppf(1 - self.ate_alpha / 2)
@@ -196,17 +185,185 @@ class BaseTLearner(object):
         return te_b
 
 
-class XGBTLearner(BaseTLearner):
+class XGBTRegressor(BaseTRegressor):
     def __init__(self, ate_alpha=.05, control_name=0, *args, **kwargs):
         """Initialize a T-learner with two XGBoost models."""
-        super(XGBTLearner, self).__init__(learner=XGBRegressor(*args, **kwargs),
+        super(XGBTRegressor, self).__init__(learner=XGBRegressor(*args, **kwargs),
                                           ate_alpha=ate_alpha,
                                           control_name=control_name)
 
 
-class MLPTLearner(BaseTLearner):
+class MLPTRegressor(BaseTRegressor):
     def __init__(self, ate_alpha=.05, control_name=0, *args, **kwargs):
         """Initialize a T-learner with two MLP models."""
-        super(MLPTLearner, self).__init__(learner=MLPRegressor(*args, **kwargs),
+        super(MLPTRegressor, self).__init__(learner=MLPRegressor(*args, **kwargs),
                                           ate_alpha=ate_alpha,
                                           control_name=control_name)
+
+
+class BaseTClassifier(object):
+    """A parent class for T-learner classifier classes.
+
+    A T-learner estimates treatment effects with two machine learning models.
+
+    Details of T-learner are available at Kunzel et al. (2018) (https://arxiv.org/abs/1706.03461).
+    """
+
+    def __init__(self, learner=None, control_learner=None, treatment_learner=None, ate_alpha=.05, control_name=0):
+        """Initialize a T-learner.
+
+        Args:
+            learner (model): a model to estimate control and treatment outcomes.
+            control_learner (model, optional): a model to estimate control outcomes
+            treatment_learner (model, optional): a model to estimate treatment outcomes
+            ate_alpha (float, optional): the confidence level alpha of the ATE estimate
+            control_name (str or int, optional): name of control group
+        """
+        assert (learner is not None) or ((control_learner is not None) and (treatment_learner is not None))
+
+        if control_learner is None:
+            self.model_c = deepcopy(learner)
+        else:
+            self.model_c = control_learner
+
+        if treatment_learner is None:
+            self.model_t = deepcopy(learner)
+        else:
+            self.model_t = treatment_learner
+
+        self.ate_alpha = ate_alpha
+        self.control_name = control_name
+
+    def __repr__(self):
+        return '{}(model_c={}, model_t={})'.format(self.__class__.__name__,
+                                                   self.model_c.__repr__(),
+                                                   self.model_t.__repr__())
+
+    @ignore_warnings(category=ConvergenceWarning)
+    def fit(self, X, treatment, y):
+        """Fit the inference model
+
+        Args:
+            X (np.matrix): a feature matrix
+            treatment (np.array): a treatment vector
+            y (np.array): an outcome vector
+        """
+        is_treatment = treatment != self.control_name
+
+        t_groups = np.unique(treatment[is_treatment])
+        self._classes = {}
+        # this should be updated for multi-treatment case
+        self._classes[t_groups[0]] = 0
+
+        logger.info('Training a control group model')
+        self.model_c.fit(X[~is_treatment], y[~is_treatment])
+
+        logger.info('Training a treatment group model')
+        self.model_t.fit(X[is_treatment], y[is_treatment])
+
+    def predict(self, X, treatment=None, y=None):
+        """Predict treatment effects.
+
+        Args:
+            X (np.matrix): a feature matrix
+            treatment (np.array): a treatment vector
+            y (np.array, optional): an optional outcome vector
+
+        Returns:
+            (numpy.ndarray): Predictions of treatment effects.
+        """
+        yhat_c = self.model_c.predict_proba(X)[:, 1]
+        yhat_t = self.model_t.predict_proba(X)[:, 1]
+
+        if (y is not None) and (treatment is not None):
+            is_treatment = treatment != self.control_name
+            logger.info('RMSE (Control): {:.6f}'.format(np.sqrt(mse(y[~is_treatment], yhat_c[~is_treatment]))))
+            logger.info(' MAE (Control): {:.6f}'.format(mae(y[~is_treatment], yhat_c[~is_treatment])))
+            logger.info('RMSE (Treatment): {:.6f}'.format(np.sqrt(mse(y[is_treatment], yhat_t[is_treatment]))))
+            logger.info(' MAE (Treatment): {:.6f}'.format(mae(y[is_treatment], yhat_t[is_treatment])))
+
+        return (yhat_t - yhat_c).reshape(-1, 1)
+
+    def fit_predict(self, X, treatment, y, return_ci=False, n_bootstraps=1000, bootstrap_size=10000, verbose=False):
+        """Fit the inference model of the T learner and predict treatment effects.
+
+        Args:
+            X (np.matrix): a feature matrix
+            treatment (np.array): a treatment vector
+            y (np.array): an outcome vector
+            return_ci (bool): whether to return confidence intervals
+            n_bootstraps (int): number of bootstrap iterations
+            bootstrap_size (int): number of samples per bootstrap
+            verbose (str): whether to output progress logs
+
+        Returns:
+            (numpy.ndarray): Predictions of treatment effects. Output dim: [n_samples, n_treatment].
+                If return_ci, returns CATE [n_samples, n_treatment], LB [n_samples, n_treatment],
+                UB [n_samples, n_treatment]
+        """
+        self.fit(X, treatment, y)
+        te = self.predict(X, treatment, y)
+
+        if not return_ci:
+            return te
+        else:
+            start = pd.datetime.today()
+            te_bootstraps = np.zeros(shape=(X.shape[0], n_bootstraps))
+            for i in range(n_bootstraps):
+                te_b = self.bootstrap(X, treatment, y, size=bootstrap_size)
+                te_bootstraps[:, i] = np.ravel(te_b)
+                if verbose:
+                    now = pd.datetime.today()
+                    lapsed = (now - start).seconds / 60
+                    logger.info('{}/{} bootstraps completed. ({:.01f} min lapsed)'.format(i + 1, n_bootstraps, lapsed))
+
+            te_lower = np.percentile(te_bootstraps, (self.ate_alpha / 2) * 100, axis=1)
+            te_upper = np.percentile(te_bootstraps, (1 - self.ate_alpha / 2) * 100, axis=1)
+
+            return (te, te_lower, te_upper)
+
+    def estimate_ate(self, X, treatment, y):
+        """Estimate the Average Treatment Effect (ATE).
+
+        Args:
+            X (np.matrix): a feature matrix
+            treatment (np.array): a treatment vector
+            y (np.array): an outcome vector
+
+        Returns:
+            The mean and confidence interval (LB, UB) of the ATE estimate.
+        """
+        is_treatment = treatment != self.control_name
+        w = is_treatment.astype(int)
+
+        self.fit(X, treatment, y)
+
+        yhat_c = self.model_c.predict(X)
+        yhat_t = self.model_t.predict(X)
+
+        te = (yhat_t - yhat_c).mean()
+        prob_treatment = float(sum(w)) / X.shape[0]
+
+        se = np.sqrt((
+            (y[~is_treatment] - yhat_c[~is_treatment]).var()
+            / (1 - prob_treatment) +
+            (y[is_treatment] - yhat_t[is_treatment]).var()
+            / prob_treatment +
+            (yhat_t - yhat_c).var()
+        ) / y.shape[0])
+
+        te_lb = te - se * norm.ppf(1 - self.ate_alpha / 2)
+        te_ub = te + se * norm.ppf(1 - self.ate_alpha / 2)
+
+        return te, te_lb, te_ub
+
+    def bootstrap(self, X, treatment, y, size=10000):
+        """Runs a single bootstrap. Fits on bootstrapped sample, then predicts on whole population."""
+
+        idxs = np.random.choice(np.arange(0, X.shape[0]), size=size)
+        X_b = X[idxs]
+        treatment_b = treatment[idxs]
+        y_b = y[idxs]
+        self.fit(X=X_b, treatment=treatment_b, y=y_b)
+        te_b = self.predict(X=X, treatment=treatment, y=y)
+        return te_b
