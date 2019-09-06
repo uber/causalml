509c3d76c88c0c8449e618a9c1f1e4a6e225d339
Jeong-Yoon Lee <jeong@uber.com>
2019-09-05 20:34:10 -0700

    update plots in metrics. add qini_score to metrics (#45)
    * add metrics.plot() that generates the lift, gain and qini plots
    * add 0 and last data point to plots
    * add qini_score() to metrics. 
    * fix scales, start & end of plots

--- causalml/metrics/__init__.py
+++ causalml/metrics/__init__.py
@@ -1,3 +1,4 @@
 from .classification import roc_auc_score, logloss, classification_metrics
 from .regression import ape, mape, mae, rmse, r2_score, gini, smape, regression_metrics
-from .visualize import plot_gain, plot_lift, plot_qini, get_cumgain, get_cumlift, get_qini, auuc_score
+from .visualize import plot, plot_gain, plot_lift, plot_qini, get_cumgain, get_cumlift, get_qini
+from .visualize import auuc_score, qini_score
