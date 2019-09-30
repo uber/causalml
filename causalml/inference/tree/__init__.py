42925fde4878bd11ec2572b62141d03ef5c10ba3
zhenyuz0500 <zhenyuz0500@gmail.com>
2019-09-30 11:47:49 -0700

    uplift tree visualization

--- causalml/inference/tree/__init__.py
+++ causalml/inference/tree/__init__.py
@@ -1,3 +1,4 @@
 from .models import UpliftTreeClassifier, DecisionTree
 from .models import UpliftRandomForestClassifier
+from .models import uplift_tree_string, uplift_tree_dotgraph
 from  .causaltree import CausalMSE, CausalTreeRegressor
