73af8710b4cdcc7636e38b37d6a6712a0425be4a
zhenyuz0500 <zhenyuz0500@gmail.com>
2019-10-15 13:43:07 -0700

    tree visualization file separation

--- causalml/inference/tree/__init__.py
+++ causalml/inference/tree/__init__.py
@@ -1,4 +1,5 @@
 from .models import UpliftTreeClassifier, DecisionTree
 from .models import UpliftRandomForestClassifier
-from .models import uplift_tree_string, uplift_tree_dotgraph
 from  .causaltree import CausalMSE, CausalTreeRegressor
+from .plot import uplift_tree_string, uplift_tree_plot
+from .utils import cat_group, cat_transform, cv_fold_index, cat_continuous, kpi_transform
