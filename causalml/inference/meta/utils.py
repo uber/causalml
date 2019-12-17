801f3d08353cb5dd4198ea7f9e83c5e85c95f850
Peter Foley <pwfoley@gmail.com>
2019-12-17 11:38:35 -0800

    Xgboost objective names (#102)
    * automatically satisfy xgboost objective name change
    
    xgboost 0.90 deprecated the reg:linear label in favor
    of reg:squarederror
    
    only xgboost 0.90 needed some changes to silence test warnings.
    xgboost master is now fixed, so 0.90 alone isn't worth the clutter
    
    * translate argument before validation
    * move more rlearner arg cleanup into utils
    * explicitly list packaging library in requirements

--- causalml/inference/meta/utils.py
+++ causalml/inference/meta/utils.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+from packaging import version
+from xgboost import __version__ as xgboost_version
+
 
 def check_treatment_vector(treatment, control_name=None):
     n_unique_treatments = np.unique(treatment).shape[0]
@@ -29,3 +32,57 @@ def check_explain_conditions(method, models, X=None, treatment=None, y=None):
     if method in ('permutation', 'shapley'):
         assert all([arr is not None for arr in (X, treatment, y)]), \
                 "X, treatment, and y must be provided if method = {}".format(method)
+
+
+def clean_xgboost_objective(objective):
+    """
+    Translate objective to be compatible with loaded xgboost version
+
+    Args
+    ----
+
+    objective : string
+        The objective to translate.
+
+    Returns
+    -------
+    The translated objective, or original if no translation was required.
+    """
+    compat_before_v83 = {'reg:squarederror': 'reg:linear'}
+    compat_v83_or_later = {'reg:linear': 'reg:squarederror'}
+    if version.parse(xgboost_version) < version.parse('0.83'):
+        if objective in compat_before_v83:
+            objective = compat_before_v83[objective]
+    else:
+        if objective in compat_v83_or_later:
+            objective = compat_v83_or_later[objective]
+    return objective
+
+
+def get_xgboost_objective_metric(objective):
+    """
+    Get the xgboost version-compatible objective and evaluation metric from a potentially version-incompatible input.
+
+    Args
+    ----
+
+    objective : string
+        An xgboost objective that may be incompatible with the installed version.
+
+    Returns
+    -------
+    A tuple with the translated objective and evaluation metric.
+    """
+    def clean_dict_keys(orig):
+        return {clean_xgboost_objective(k): v for (k, v) in orig.items()}
+
+    metric_mapping = clean_dict_keys({
+        'rank:pairwise': 'auc',
+        'reg:squarederror': 'rmse',
+    })
+
+    objective = clean_xgboost_objective(objective)
+
+    assert (objective in metric_mapping), \
+        'Effect learner objective must be one of: ' + ", ".join(metric_mapping)
+    return objective, metric_mapping[objective]
