789195417bd6ebf3365f59f3a9ed91b3e8d5a578
Mike Yung <mike.yung@uber.com>
2020-01-24 10:56:28 -0800

    create dragonnet class and move helper functions to utils.py; add more docstrings

---
+++ causalml/inference/nn/utils.py
@@ -0,0 +1,97 @@
+import tensorflow as tf
+import keras.backend as K
+from keras.engine.topology import Layer
+from keras.metrics import binary_accuracy
+
+
+def binary_classification_loss(concat_true, concat_pred):
+    t_true = concat_true[:, 1]
+    t_pred = concat_pred[:, 2]
+    t_pred = (t_pred + 0.001) / 1.002
+    losst = tf.reduce_sum(K.binary_crossentropy(t_true, t_pred))
+
+    return losst
+
+
+def regression_loss(concat_true, concat_pred):
+    y_true = concat_true[:, 0]
+    t_true = concat_true[:, 1]
+
+    y0_pred = concat_pred[:, 0]
+    y1_pred = concat_pred[:, 1]
+
+    loss0 = tf.reduce_sum((1. - t_true) * tf.square(y_true - y0_pred))
+    loss1 = tf.reduce_sum(t_true * tf.square(y_true - y1_pred))
+
+    return loss0 + loss1
+
+
+def ned_loss(concat_true, concat_pred):
+    t_true = concat_true[:, 1]
+
+    t_pred = concat_pred[:, 1]
+    return tf.reduce_sum(K.binary_crossentropy(t_true, t_pred))
+
+
+def dead_loss(concat_true, concat_pred):
+    return regression_loss(concat_true, concat_pred)
+
+
+def dragonnet_loss_binarycross(concat_true, concat_pred):
+    return regression_loss(concat_true, concat_pred) + binary_classification_loss(concat_true, concat_pred)
+
+
+def treatment_accuracy(concat_true, concat_pred):
+    t_true = concat_true[:, 1]
+    t_pred = concat_pred[:, 2]
+    return binary_accuracy(t_true, t_pred)
+
+
+def track_epsilon(concat_true, concat_pred):
+    epsilons = concat_pred[:, 3]
+    return tf.abs(tf.reduce_mean(epsilons))
+
+
+def make_tarreg_loss(ratio=1., dragonnet_loss=dragonnet_loss_binarycross):
+    def tarreg_ATE_unbounded_domain_loss(concat_true, concat_pred):
+        vanilla_loss = dragonnet_loss(concat_true, concat_pred)
+
+        y_true = concat_true[:, 0]
+        t_true = concat_true[:, 1]
+
+        y0_pred = concat_pred[:, 0]
+        y1_pred = concat_pred[:, 1]
+        t_pred = concat_pred[:, 2]
+
+        epsilons = concat_pred[:, 3]
+        t_pred = (t_pred + 0.01) / 1.02
+        # t_pred = tf.clip_by_value(t_pred,0.01, 0.99,name='t_pred')
+
+        y_pred = t_true * y1_pred + (1 - t_true) * y0_pred
+
+        h = t_true / t_pred - (1 - t_true) / (1 - t_pred)
+
+        y_pert = y_pred + epsilons * h
+        targeted_regularization = tf.reduce_sum(tf.square(y_true - y_pert))
+
+        # final
+        loss = vanilla_loss + ratio * targeted_regularization
+        return loss
+
+    return tarreg_ATE_unbounded_domain_loss
+
+
+class EpsilonLayer(Layer):
+    def __init__(self):
+        super(EpsilonLayer, self).__init__()
+
+    def build(self, input_shape):
+        # Create a trainable weight variable for this layer.
+        self.epsilon = self.add_weight(name='epsilon',
+                                       shape=[1, 1],
+                                       initializer='RandomNormal',
+                                       trainable=True)
+        super(EpsilonLayer, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, inputs, **kwargs):
+        return self.epsilon * tf.ones_like(inputs)[:, 0:1]
