9d52527086cfe9e63247c3d5e3407573317add7d
ppstacy <ppstacy@gmail.com>
2020-06-24 11:33:31 -0700

    Sensitivity analysis (#199)
    * add the sensitivity analysis methods and example notebook

--- causalml/metrics/__init__.py
+++ causalml/metrics/__init__.py
@@ -3,3 +3,5 @@ from .regression import ape, mape, mae, rmse, r2_score, gini, smape, regression_
 from .visualize import plot, plot_gain, plot_lift, plot_qini, plot_tmlegain, plot_tmleqini # noqa
 from .visualize import get_cumgain, get_cumlift, get_qini, get_tmlegain, get_tmleqini # noqa
 from .visualize import auuc_score, qini_score # noqa
+from .sensitivity import Sensitivity, SensitivityPlaceboTreatment # noqa
+from .sensitivity import SensitivityRandomCause, SensitivityRandomReplace, SensitivitySubsetData, SensitivitySelectionBias # noqa
