ab8d271d7e524024444b862256ebc042d3987387
Paul Lo <paul.lo@uber.com>
2020-10-21 13:57:34 -0700

    Fix #241 by putting back pandas Series as an acceptable format for propensity score parameter

--- causalml/inference/meta/xlearner.py
+++ causalml/inference/meta/xlearner.py
@@ -245,7 +245,7 @@ class BaseXLearner(object):
             p = self.propensity
 
         check_p_conditions(p, self.t_groups)
-        if isinstance(p, np.ndarray):
+        if isinstance(p, (np.ndarray, pd.Series)):
             treatment_name = self.t_groups[0]
             p = {treatment_name: convert_pd_to_np(p)}
         elif isinstance(p, dict):
@@ -305,7 +305,7 @@ class BaseXLearner(object):
             p = self.propensity
         else:
             check_p_conditions(p, self.t_groups)
-        if isinstance(p, np.ndarray):
+        if isinstance(p, (np.ndarray, pd.Series)):
             treatment_name = self.t_groups[0]
             p = {treatment_name: convert_pd_to_np(p)}
         elif isinstance(p, dict):
