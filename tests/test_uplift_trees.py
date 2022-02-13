fb3c8c19002fc82f7a857a18f304ca9089795556
Florian Heiderich <florian@heiderich.org>
2022-02-13 18:29:34 +0100

    test predictions coincide for single and parallel
    computation for UpliftRandomForestClassifier

--- tests/test_uplift_trees.py
+++ tests/test_uplift_trees.py
@@ -36,7 +36,21 @@ def test_UpliftRandomForestClassifier(generate_classification_data, backend):
                          treatment=df_train['treatment_group_key'].values,
                          y=df_train[CONVERSION].values)
 
-        y_pred = uplift_model.predict(df_test[x_names].values)
+        predictions = {}
+        predictions["single"] = uplift_model.predict(df_test[x_names].values)
+        with parallel_backend("loky", n_jobs=2):
+            predictions["loky_2"] = uplift_model.predict(df_test[x_names].values)
+        with parallel_backend("threading", n_jobs=2):
+            predictions["threading_2"] = uplift_model.predict(df_test[x_names].values)
+        with parallel_backend("multiprocessing", n_jobs=2):
+            predictions["multiprocessing_2"] = uplift_model.predict(df_test[x_names].values)
+
+        # assert that the predictions coincide for single and all parallel computations
+        iterator = iter(predictions.values())
+        first = next(iterator)
+        assert all(np.array_equal(first, rest) for rest in iterator)
+
+        y_pred = list(predictions.values())[0]
         result = pd.DataFrame(y_pred, columns=uplift_model.classes_[1:])
 
         best_treatment = np.where((result < 0).all(axis=1),
