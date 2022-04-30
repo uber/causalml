17357a90834bd7c5986ed6d588c6a0a1c98353d3
paullo0106 <paullo0106@gmail.com>
2022-04-30 00:53:58 -0700

    Fix Lint/ Black for coding style compliace

--- tests/test_feature_selection.py
+++ tests/test_feature_selection.py
@@ -11,16 +11,17 @@ def test_filter_f(generate_classification_data):
     y_name = CONVERSION
 
     # test F filter
-    method = 'F'
+    method = "F"
     filter_f = FilterSelect()
-    f_imp = filter_f.get_importance(df, X_names, y_name, method,
-                                    treatment_group='treatment1')
+    f_imp = filter_f.get_importance(
+        df, X_names, y_name, method, treatment_group="treatment1"
+    )
 
     # each row represents the rank and importance score of each feature
     # and spot check if it's sorted properly
     assert f_imp.shape[0] == len(X_names)
-    assert f_imp['rank'].values[0] == 1
-    assert f_imp['score'].values[0] >= f_imp['score'].values[1]
+    assert f_imp["rank"].values[0] == 1
+    assert f_imp["score"].values[0] >= f_imp["score"].values[1]
 
 
 def test_filter_lr(generate_classification_data):
@@ -30,16 +31,17 @@ def test_filter_lr(generate_classification_data):
     y_name = CONVERSION
 
     # test LR filter
-    method = 'LR'
+    method = "LR"
     filter_obj = FilterSelect()
-    imp = filter_obj.get_importance(df, X_names, y_name, method,
-                                    treatment_group='treatment1')
+    imp = filter_obj.get_importance(
+        df, X_names, y_name, method, treatment_group="treatment1"
+    )
 
     # each row represents the rank and importance score of each feature
     # and spot check if it's sorted properly
     assert imp.shape[0] == len(X_names)
-    assert imp['rank'].values[0] == 1
-    assert imp['score'].values[0] >= imp['score'].values[1]
+    assert imp["rank"].values[0] == 1
+    assert imp["score"].values[0] >= imp["score"].values[1]
 
 
 def test_filter_kl(generate_classification_data):
@@ -49,13 +51,14 @@ def test_filter_kl(generate_classification_data):
     y_name = CONVERSION
 
     # test KL filter
-    method = 'KL'
+    method = "KL"
     filter_obj = FilterSelect()
-    imp = filter_obj.get_importance(df, X_names, y_name, method,
-                                    treatment_group='treatment1')
+    imp = filter_obj.get_importance(
+        df, X_names, y_name, method, treatment_group="treatment1"
+    )
 
     # each row represents the rank and importance score of each feature
     # and spot check if it's sorted properly
     assert imp.shape[0] == len(X_names)
-    assert imp['rank'].values[0] == 1
-    assert imp['score'].values[0] >= imp['score'].values[1]
+    assert imp["rank"].values[0] == 1
+    assert imp["score"].values[0] >= imp["score"].values[1]
