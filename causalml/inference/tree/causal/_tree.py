c360ec824a78220c9d42444c0e371b950ca3e8b3
volico <ieavilov@gmail.com>
2022-11-10 08:02:58 +0300

    t test criterion

--- causalml/inference/tree/causal/_tree.py
+++ causalml/inference/tree/causal/_tree.py
@@ -15,9 +15,13 @@ from sklearn.tree._splitter import Splitter
 from sklearn.utils.validation import _check_sample_weight
 
 from .builder import DepthFirstCausalTreeBuilder
-from .criterion import StandardMSE, CausalMSE
+from .criterion import StandardMSE, CausalMSE, TTest
 
-CAUSAL_TREES_CRITERIA = {"causal_mse": CausalMSE, "standard_mse": StandardMSE}
+CAUSAL_TREES_CRITERIA = {
+    "causal_mse": CausalMSE,
+    "standard_mse": StandardMSE,
+    "t_test": TTest,
+}
 CRITERIA_REG.update(CAUSAL_TREES_CRITERIA)
 
 
