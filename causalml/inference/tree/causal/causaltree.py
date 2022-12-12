Causal trees bootstrapping and `max_leaf_nodes` fixes with minor update (#583)
* Add bootstrap fix for causal forest fit
* Black reformat
* Fix max_leaf_nodes behaviour with BestFirstCausalTreeBuilder
* Add penalty for treatment and control samples distribution
* Add treatment and control info to tree builders
* Manage criterias public variables via BaseCausalDecisionTree
* Add groups info to causal tree plot
* Update CausalTreRegressor and CausalRandomForestRegressor interfaces
* Update causal trees notebooks in examples
* Black code reformat
* Update causal trees tests
* Remove Literal for python=3.7 support
* Fix setup_requires

causalml/inference/tree/causal/causaltree.py
@@ -2,13 +2,11 @@ import logging
 import sys
 from typing import Union
 
-import forestci as fci
 import numpy as np
 import tqdm
 from pathos.pools import ProcessPool as PPool
 from scipy.stats import norm
 from sklearn.base import RegressorMixin
-from sklearn.ensemble._forest import ForestRegressor
 from sklearn.tree._tree import DTYPE
 from sklearn.utils import check_array
 from sklearn.utils.validation import check_is_fitted
@@ -34,15 +32,17 @@ class CausalTreeRegressor(RegressorMixin, BaseCausalDecisionTree):
         alpha: float = 0.05,
         control_name: Union[int, str] = 0,
         max_depth: int = None,
-        min_samples_split: Union[int, float] = 2,
+        min_samples_split: Union[int, float] = 60,
         min_weight_fraction_leaf: float = 0.0,
         max_features: Union[int, float, str] = None,
         max_leaf_nodes: int = None,
         min_impurity_decrease: float = float("-inf"),
         ccp_alpha: float = 0.0,
+        groups_penalty: float = 0.5,
         min_samples_leaf: int = 100,
         random_state: int = None,
-        leaves_groups_cnt: bool = False,
+        groups_cnt: bool = False,
+        groups_cnt_mode: str = "nodes",
     ):
         """
         Initialize a Causal Tree
@@ -92,6 +92,9 @@ class CausalTreeRegressor(RegressorMixin, BaseCausalDecisionTree):
                 subtree with the largest cost complexity that is smaller than
                 ``ccp_alpha`` will be chosen. By default, no pruning is performed. See
                 :ref:`minimal_cost_complexity_pruning` for details.
+            groups_penalty: (float, default=0.5)
+                This penalty coefficient manages the node impurity increase in case of the difference between
+                treatment and control samples sizes.
             min_samples_leaf: (int or float), default=100
                 The minimum number of samples required to be at a leaf node.
                 A split point at any depth will only be considered if it leaves at
@@ -106,7 +109,8 @@ class CausalTreeRegressor(RegressorMixin, BaseCausalDecisionTree):
             random_state: (int), RandomState instance or None, default=None
                 Used to pick randomly the `max_features` used at each split.
                 See :term:`Glossary <random_state>` for details.
-            leaves_groups_cnt: (bool), count treatment groups for each leaf
+            groups_cnt: (bool), count treatment and control groups for each node/leaf
+            groups_cnt_mode: (str, 'nodes', 'leaves'), mode for samples counting
         """
 
         self.criterion = criterion
@@ -120,13 +124,15 @@ class CausalTreeRegressor(RegressorMixin, BaseCausalDecisionTree):
         self.max_leaf_nodes = max_leaf_nodes
         self.min_impurity_decrease = min_impurity_decrease
         self.ccp_alpha = ccp_alpha
+        self.groups_penalty = groups_penalty
         self.min_samples_leaf = min_samples_leaf
         self.random_state = random_state
 
         self.eps = 1e-5
         self._classes = {}
-        self.leaves_groups_cnt = leaves_groups_cnt
-        self._leaves_groups_cnt = {}
+        self.groups_cnt = groups_cnt
+        self.groups_cnt_mode = groups_cnt_mode
+        self._groups_cnt = {}
 
         super().__init__(
             criterion=criterion,
@@ -176,17 +182,12 @@ class CausalTreeRegressor(RegressorMixin, BaseCausalDecisionTree):
             X, y, w = X, y, sample_weight
         else:
             X, y, w = self._prepare_data(X=X, y=y, treatment=treatment)
+        self.treatment_groups = np.unique(w)
 
         super().fit(X=X, y=y, sample_weight=self.eps + w, check_input=check_input)
 
-        if self.leaves_groups_cnt:
-            if treatment is None:
-                raise ValueError(
-                    "`treatment` must be provided to get _leaves_groups_cnt attribute."
-                )
-            self._leaves_groups_cnt = self._count_groups_distribution(
-                X=X, treatment=treatment
-            )
+        if self.groups_cnt:
+            self._groups_cnt = self._count_groups_distribution(X=X, treatment=w)
         return self
 
     def fit_predict(
@@ -355,8 +356,6 @@ class CausalTreeRegressor(RegressorMixin, BaseCausalDecisionTree):
             )
         check_treatment_vector(treatment, self.control_name)
 
-        self.treatment_groups = np.unique(treatment)
-
         self.is_treatment = treatment != self.control_name
         w = self.is_treatment.astype(int)
 
@@ -367,234 +366,35 @@ class CausalTreeRegressor(RegressorMixin, BaseCausalDecisionTree):
 
         return X, y, w
 
-    def _get_leaf_id(self, X: np.ndarray) -> int:
-        """
-        Find tree leaf id for a feature vector
-        Args:
-            X: (np.ndarray), feature vector
-        Returns: int, leaf id
-        """
-        check_is_fitted(self)
-        assert (
-            X.shape[0] == self.n_features
-        ), f"Trained model has different feature size. Got: {X.shape[0]}. Expected:{self.n_features}"
-
-        node_id = 0
-        while not self.is_leaves[node_id]:
-
-            left_node_id = self.tree_.children_left[node_id]
-            right_node_id = self.tree_.children_right[node_id]
-            feature_value = X[self.tree_.feature[node_id]]
-
-            if feature_value <= self.tree_.threshold[node_id]:
-                node_id = left_node_id
-            else:
-                node_id = right_node_id
-        return node_id
-
     def _count_groups_distribution(self, X: np.ndarray, treatment: np.ndarray) -> dict:
 
         """
-        Count treatment, control distribution for tree leaves based on train data
+        Count treatment, control distribution for tree nodes/leaves
         Args:
             X: (np.ndarray), feature matrix
             treatment: (np.ndarray), treatment vector
         Returns:
-            dict: treatment groups for each tree leaf
+            dict: treatment groups for each tree node/leaves
         """
         check_is_fitted(self)
 
         self.is_leaves = get_tree_leaves_mask(self)
-        nodes_ids = np.array(range(self.tree_.node_count))
-        leaves_ids = nodes_ids[self.is_leaves]
-
-        leaves_groups_cnt = {
-            leaf_id: {group: 0 for group in self.treatment_groups}
-            for leaf_id in leaves_ids
+        groups_cnt = {
+            idx: {group: 0 for group in self.treatment_groups}
+            for idx in np.array(range(self.tree_.node_count))
         }
+        node_indicators = self.tree_.decision_path(X.astype(np.float32))
 
-        for row_id in range(X.shape[0]):
-            leaf_id = self._get_leaf_id(X=X[row_id, :])
-            leaves_groups_cnt[leaf_id][treatment[row_id]] += 1
+        for sample_id in range(X.shape[0]):
+            nodes_path = node_indicators.indices[
+                node_indicators.indptr[sample_id] : node_indicators.indptr[
+                    sample_id + 1
+                ]
+            ]
 
-        return leaves_groups_cnt
-
-
-class CausalRandomForestRegressor(ForestRegressor):
-    def __init__(
-        self,
-        n_estimators: int = 100,
-        *,
-        control_name: Union[int, str] = 0,
-        criterion: str = "causal_mse",
-        alpha: float = 0.05,
-        max_depth: int = None,
-        min_samples_split: int = 2,
-        min_samples_leaf: int = 100,
-        min_weight_fraction_leaf: float = 0.0,
-        max_features: Union[int, float, str] = 1.0,
-        max_leaf_nodes: int = None,
-        min_impurity_decrease: float = float("-inf"),
-        bootstrap: bool = True,
-        oob_score: bool = False,
-        n_jobs: int = None,
-        random_state: int = None,
-        verbose: int = 0,
-        warm_start: bool = False,
-        ccp_alpha: float = 0.0,
-        max_samples: int = None,
-    ):
-        """
-        Initialize Random Forest of CausalTreeRegressors
-
-        Args:
-            n_estimators: (int, default=100)
-                    Number of trees in the forest
-            control_name: (str or int)
-                    Name of control group
-            criterion: ({"causal_mse", "standard_mse"}, default="causal_mse"):
-                    Function to measure the quality of a split.
-            alpha: (float)
-                    The confidence level alpha of the ATE estimate and ITE bootstrap estimates
-            max_depth: (int, default=None)
-                    The maximum depth of the tree.
-            min_samples_split: (int or float, default=2)
-                The minimum number of samples required to split an internal node:
-            min_samples_leaf: (int or float), default=100
-                The minimum number of samples required to be at a leaf node.
-            min_weight_fraction_leaf: (float, default=0.0)
-                The minimum weighted fraction of the sum total of weights (of all
-                the input samples) required to be at a leaf node.
-            max_features: (int, float or {"auto", "sqrt", "log2"}, default=None)
-                The number of features to consider when looking for the best split
-            max_leaf_nodes: (int, default=None)
-                Grow a tree with ``max_leaf_nodes`` in best-first fashion.
-            min_impurity_decrease: (float, default=float("-inf")))
-                A node will be split if this split induces a decrease of the impurity
-                greater than or equal to this value.
-            bootstrap : (bool, default=True)
-                Whether bootstrap samples are used when building trees.
-            oob_score : bool, default=False
-                Whether to use out-of-bag samples to estimate the generalization score.
-            n_jobs : int, default=None
-                    The number of jobs to run in parallel.
-            random_state : (int, RandomState instance or None, default=None)
-                    Controls both the randomness of the bootstrapping of the samples used
-                    when building trees (if ``bootstrap=True``) and the sampling of the
-                    features to consider when looking for the best split at each node
-                    (if ``max_features < n_features``).
-            verbose : (int, default=0)
-                    Controls the verbosity when fitting and predicting.
-            warm_start : (bool, default=False)
-                    When set to ``True``, reuse the solution of the previous call to fit
-                    and add more estimators to the ensemble, otherwise, just fit a whole
-                    new forest.
-            ccp_alpha : (non-negative float, default=0.0)
-                    Complexity parameter used for Minimal Cost-Complexity Pruning.
-            max_samples : (int or float, default=None)
-                    If bootstrap is True, the number of samples to draw from X
-                    to train each base estimator.
-        """
-        super().__init__(
-            base_estimator=CausalTreeRegressor(
-                control_name=control_name, criterion=criterion
-            ),
-            n_estimators=n_estimators,
-            estimator_params=(
-                "criterion",
-                "control_name",
-                "max_depth",
-                "min_samples_split",
-                "min_weight_fraction_leaf",
-                "max_features",
-                "max_leaf_nodes",
-                "min_impurity_decrease",
-                "ccp_alpha",
-                "min_samples_leaf",
-                "random_state",
-            ),
-            bootstrap=bootstrap,
-            oob_score=oob_score,
-            n_jobs=n_jobs,
-            random_state=random_state,
-            verbose=verbose,
-            warm_start=warm_start,
-            max_samples=max_samples,
-        )
-
-        self.criterion = criterion
-        self.control_name = control_name
-        self.max_depth = max_depth
-        self.min_samples_split = min_samples_split
-        self.min_samples_leaf = min_samples_leaf
-        self.min_weight_fraction_leaf = min_weight_fraction_leaf
-        self.max_features = max_features
-        self.max_leaf_nodes = max_leaf_nodes
-        self.min_impurity_decrease = min_impurity_decrease
-        self.ccp_alpha = ccp_alpha
-        self.alpha = alpha
-
-    def fit(self, X: np.ndarray, treatment: np.ndarray, y: np.ndarray):
-        """
-        Fit Causal RandomForest
-        Args:
-            X: (np.ndarray), feature matrix
-            treatment: (np.ndarray), treatment vector
-            y: (np.ndarray), outcome vector
-        Returns:
-             self
-        """
-        X, y, w = self.base_estimator._prepare_data(X=X, y=y, treatment=treatment)
-        return super().fit(X=X, y=y, sample_weight=w)
-
-    def calculate_error(
-        self,
-        X_train: np.ndarray,
-        X_test: np.ndarray,
-        inbag: np.ndarray = None,
-        calibrate: bool = True,
-        memory_constrained: bool = False,
-        memory_limit: int = None,
-    ) -> np.ndarray:
-        """
-        Calculate error bars from scikit-learn RandomForest estimators
-        Source:
-        https://github.com/scikit-learn-contrib/forest-confidence-interval
-
-        Args:
-            X_train: (np.ndarray), training subsample of feature matrix, (n_train_sample, n_features)
-            X_test: (np.ndarray), test subsample of feature matrix, (n_train_sample, n_features)
-            inbag: (ndarray, optional),
-                    The inbag matrix that fit the data. If set to `None` (default) it
-                    will be inferred from the forest. However, this only works for trees
-                    for which bootstrapping was set to `True`. That is, if sampling was
-                    done with replacement. Otherwise, users need to provide their own
-                    inbag matrix.
-            calibrate: (boolean, optional)
-                    Whether to apply calibration to mitigate Monte Carlo noise.
-                    Some variance estimates may be negative due to Monte Carlo effects if
-                    the number of trees in the forest is too small. To use calibration,
-                    Default: True
-            memory_constrained: (boolean, optional)
-                    Whether or not there is a restriction on memory. If False, it is
-                    assumed that a ndarray of shape (n_train_sample,n_test_sample) fits
-                    in main memory. Setting to True can actually provide a speedup if
-                    memory_limit is tuned to the optimal range.
-            memory_limit: (int, optional)
-                    An upper bound for how much memory the intermediate matrices will take
-                    up in Megabytes. This must be provided if memory_constrained=True.
-
-        Returns:
-            (np.ndarray), An array with the unbiased sampling variance for a RandomForest object.
-        """
-
-        var = fci.random_forest_error(
-            self,
-            X_train,
-            X_test,
-            inbag=inbag,
-            calibrate=calibrate,
-            memory_constrained=memory_constrained,
-            memory_limit=memory_limit,
-        )
-        return var
+            if self.groups_cnt_mode == "leaves":
+                groups_cnt[nodes_path[-1]][treatment[sample_id]] += 1
+            elif self.groups_cnt_mode == "nodes":
+                for node_id in nodes_path:
+                    groups_cnt[node_id][treatment[sample_id]] += 1
+        return groups_cnt

causalml/inference/tree/plot.py
@@ -10,6 +10,10 @@ import numpy as np
 import pydotplus
 import seaborn as sns
 
+from sklearn.tree import _tree
+from sklearn.tree._export import _MPLTreeExporter, _color_brew
+from sklearn.utils.validation import check_is_fitted
+
 from .utils import get_tree_leaves_mask
 from . import CausalTreeRegressor
 
@@ -288,14 +292,18 @@ def uplift_tree_plot(decisionTree, x_names):
 
 
 def plot_dist_tree_leaves_values(
-    tree: CausalTreeRegressor, title: str = "Leaves values distribution"
+    tree: CausalTreeRegressor,
+    title: str = "Leaves values distribution",
+    figsize: tuple = (5, 5),
+    fontsize: int = 12,
 ) -> None:
     """
     Create distplot for tree leaves values
     Args:
         tree: (CausalTreeRegressor), Tree object
-        figsize: (tuple), figure size
         title: (str), plot title
+        figsize: (tuple), figure size
+        fontsize: (int), title font size
 
     Returns: None
 
@@ -304,6 +312,345 @@ def plot_dist_tree_leaves_values(
     leaves_values = tree.tree_.value.reshape(
         -1,
     )[tree_leaves_mask]
-    sns.distplot(leaves_values)
-    plt.title(title)
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.distplot(
+        leaves_values,
+        ax=ax,
+    )
+    plt.title(title, fontsize=fontsize)
     plt.show()
+
+
+class _MPLCTreeExporter(_MPLTreeExporter):
+    def __init__(
+        self,
+        causal_tree: CausalTreeRegressor,
+        max_depth: int,
+        feature_names: list,
+        class_names: list,
+        label: str,
+        filled: bool,
+        impurity: bool,
+        groups_count: bool,
+        treatment_groups: tuple,
+        node_ids: bool,
+        proportion: bool,
+        rounded: bool,
+        precision: int,
+        fontsize: int,
+    ):
+        """
+        Causal Tree exporter for matplotlib
+        Source: https://github.com/scikit-learn/scikit-learn/blob/1.0.X/sklearn/tree/_export.py
+        Args:
+            causal_tree: CausalTreeRegressor
+                    The causal tree to be plotted
+             max_depth:  int, default=None
+                The maximum depth of the representation. If None, the tree is fully generated.
+            feature_names: list of strings, default=None
+                    Names of each of the features.
+                    If None, generic names will be used ("X[0]", "X[1]", ...).
+            class_names:    list of str or bool, default=None
+                    Names of each of the target classes in ascending numerical order.
+                    Only relevant for classification and not supported for multi-output.
+                    If ``True``, shows a symbolic representation of the class name.
+            label:  {'all', 'root', 'none'}, default='all'
+                    Whether to show informative labels for impurity, etc.
+                    Options include 'all' to show at every node, 'root' to show only at
+                    the top root node, or 'none' to not show at any node.
+            filled: bool, default=False
+                    When set to ``True``, paint nodes to indicate extremity of node values
+            impurity: bool, default=True
+                    When set to ``True``, show the impurity at each node.
+             groups_count: bool, default=True
+                Add the number of treatment and control groups
+            treatment_groups: tuple, default=(0, 1)
+                    Treatment and control groups labels
+            node_ids: bool, default=False
+                    When set to ``True``, show the ID number on each node.
+            proportion: bool, default=False
+                When set to ``True``, change the display of 'values' and/or 'samples'
+                to be proportions and percentages respectively.
+            rounded: bool, default=False
+                    When set to ``True``, draw node boxes with rounded corners and use
+                    Helvetica fonts instead of Times-Roman.
+            precision: int, default=3
+                    Number of digits of precision for floating point in the values of
+                    impurity, threshold and value attributes of each node.
+            fontsize: int, default=None
+                    Size of text font. If None, determined automatically to fit figure.
+        """
+        super().__init__(
+            max_depth,
+            feature_names,
+            class_names,
+            label,
+            filled,
+            impurity,
+            node_ids,
+            proportion,
+            rounded,
+            precision,
+            fontsize,
+        )
+        self.causal_tree = causal_tree
+        self.groups_count = groups_count
+        self.treatment_groups = treatment_groups
+
+    def node_to_str(
+        self, tree: _tree.Tree, node_id: int, criterion: str or object
+    ) -> str:
+        """
+        Generate the node content string
+        Args:
+            tree:      Tree class
+            node_id:   int, Tree node id
+            criterion: str or object, split criterion
+        Returns: str, node content
+        """
+        if tree.n_outputs == 1:
+            value = tree.value[node_id][0, :]
+        else:
+            value = tree.value[node_id]
+
+        # Should labels be shown?
+        labels = (self.label == "root" and node_id == 0) or self.label == "all"
+
+        characters = self.characters
+        node_string = characters[-1]
+
+        # Write node ID
+        if self.node_ids:
+            if labels:
+                node_string += "node "
+            node_string += characters[0] + str(node_id) + characters[4]
+
+        # Write decision criteria
+        if tree.children_left[node_id] != _tree.TREE_LEAF:
+            # Always write node decision criteria, except for leaves
+            if self.feature_names is not None:
+                feature = self.feature_names[tree.feature[node_id]]
+            else:
+                feature = "X%s%s%s" % (
+                    characters[1],
+                    tree.feature[node_id],
+                    characters[2],
+                )
+            node_string += "%s %s %s%s" % (
+                feature,
+                characters[3],
+                round(tree.threshold[node_id], self.precision),
+                characters[4],
+            )
+
+        # Write impurity
+        if self.impurity:
+            if not isinstance(criterion, str):
+                criterion = "impurity"
+            if labels:
+                node_string += "%s = " % criterion
+            node_string += (
+                str(round(tree.impurity[node_id], self.precision)) + characters[4]
+            )
+
+        # Write node sample count
+        if labels:
+            node_string += "samples = "
+        if self.proportion:
+            percent = (
+                100.0 * tree.n_node_samples[node_id] / float(tree.n_node_samples[0])
+            )
+            node_string += str(round(percent, 1)) + "%" + characters[4]
+        else:
+            node_string += str(tree.n_node_samples[node_id]) + characters[4]
+
+        # Write the number of samples per treatment and control groups
+        if self.groups_count:
+            for group in self.treatment_groups:
+                node_string += (
+                    f"Group {group} = {self.causal_tree._groups_cnt[node_id][group]} "
+                )
+        node_string += characters[4]
+
+        # Write node class distribution / regression value
+        if self.proportion and tree.n_classes[0] != 1:
+            # For classification this will show the proportion of samples
+            value = value / tree.weighted_n_node_samples[node_id]
+        if labels:
+            node_string += "value = "
+        if tree.n_classes[0] == 1:
+            # Regression
+            value_text = np.around(value, self.precision)
+        elif self.proportion:
+            # Classification
+            value_text = np.around(value, self.precision)
+        elif np.all(np.equal(np.mod(value, 1), 0)):
+            # Classification without floating-point weights
+            value_text = value.astype(int)
+        else:
+            # Classification with floating-point weights
+            value_text = np.around(value, self.precision)
+        # Strip whitespace
+        value_text = str(value_text.astype("S32")).replace("b'", "'")
+        value_text = value_text.replace("' '", ", ").replace("'", "")
+        if tree.n_classes[0] == 1 and tree.n_outputs == 1:
+            value_text = value_text.replace("[", "").replace("]", "")
+        value_text = value_text.replace("\n ", characters[4])
+        node_string += value_text + characters[4]
+
+        # Write node majority class
+        if (
+            self.class_names is not None
+            and tree.n_classes[0] != 1
+            and tree.n_outputs == 1
+        ):
+            # Only done for single-output classification trees
+            if labels:
+                node_string += "class = "
+            if self.class_names is not True:
+                class_name = self.class_names[np.argmax(value)]
+            else:
+                class_name = "y%s%s%s" % (
+                    characters[1],
+                    np.argmax(value),
+                    characters[2],
+                )
+            node_string += class_name
+
+        # Clean up any trailing newlines
+        if node_string.endswith(characters[4]):
+            node_string = node_string[: -len(characters[4])]
+
+        return node_string + characters[5]
+
+    def get_color(self, value: np.ndarray) -> str:
+        """
+        Compute HTML color for a Tree node
+        Args:
+            value: Tree node value
+        Returns: str, html color code in #RRGGBB format
+        """
+        # Regression tree or multi-output
+        color = list(self.colors["rgb"][0])
+        alpha = float(value - self.colors["bounds"][0]) / (
+            self.colors["bounds"][1] - self.colors["bounds"][0]
+        )
+        alpha = 0 if np.isnan(alpha) else alpha
+        # Compute the color as alpha against white
+        color = [int(round(alpha * c + (1 - alpha) * 255, 0)) for c in color]
+        return "#%2x%2x%2x" % tuple(color)
+
+    def get_fill_color(self, tree: _tree.Tree, node_id: int) -> str:
+        """
+         Fetch appropriate color for node
+        Args:
+            tree:    Tree class
+            node_id: int, node index
+        Returns: str
+        """
+        if "rgb" not in self.colors:
+            # Initialize colors and bounds if required
+            self.colors["rgb"] = _color_brew(tree.n_classes[0])
+            if tree.n_outputs != 1:
+                # Find max and min impurities for multi-output
+                self.colors["bounds"] = (
+                    np.nanmin(-tree.impurity),
+                    np.nanmax(-tree.impurity),
+                )
+            elif tree.n_classes[0] == 1 and len(np.unique(tree.value)) != 1:
+                # Find max and min values in leaf nodes for regression
+                self.colors["bounds"] = (np.nanmin(tree.value), np.nanmax(tree.value))
+        if tree.n_outputs == 1:
+            node_val = tree.value[node_id][0, :] / tree.weighted_n_node_samples[node_id]
+            if tree.n_classes[0] == 1:
+                # Regression
+                node_val = tree.value[node_id][0, :]
+        else:
+            # If multi-output color node by impurity
+            node_val = -tree.impurity[node_id]
+        return self.get_color(node_val)
+
+
+def plot_causal_tree(
+    causal_tree: CausalTreeRegressor,
+    *,
+    max_depth: int = None,
+    feature_names: list = None,
+    class_names: list = None,
+    label: str = "all",
+    filled: bool = False,
+    impurity: bool = True,
+    groups_count: bool = True,
+    treatment_groups: tuple = (0, 1),
+    node_ids: bool = False,
+    proportion: bool = False,
+    rounded: bool = False,
+    precision: int = 3,
+    ax: plt.Axes = None,
+    fontsize: int = None,
+):
+    """
+    Plot a Causal Tree.
+    Source: https://github.com/scikit-learn/scikit-learn/blob/1.0.X/sklearn/tree/_export.py
+    Args:
+        causal_tree: CausalTreeRegressor
+                The causal tree to be plotted
+        max_depth:  int, default=None
+                The maximum depth of the representation. If None, the tree is fully generated.
+        feature_names: list of strings, default=None
+                Names of each of the features.
+                If None, generic names will be used ("X[0]", "X[1]", ...).
+        class_names:    list of str or bool, default=None
+                Names of each of the target classes in ascending numerical order.
+                Only relevant for classification and not supported for multi-output.
+                If ``True``, shows a symbolic representation of the class name.
+        label:  {'all', 'root', 'none'}, default='all'
+                Whether to show informative labels for impurity, etc.
+                Options include 'all' to show at every node, 'root' to show only at
+                the top root node, or 'none' to not show at any node.
+        filled: bool, default=False
+                When set to ``True``, paint nodes to indicate extremity of node values
+        impurity: bool, default=True
+                When set to ``True``, show the impurity at each node.
+        groups_count: bool, default=True
+                Add the number of treatment and control groups
+        treatment_groups: tuple, default=(0, 1)
+                Treatment and control groups labels
+        node_ids: bool, default=False
+                When set to ``True``, show the ID number on each node.
+        proportion: bool, default=False
+                When set to ``True``, change the display of 'values' and/or 'samples'
+                to be proportions and percentages respectively.
+        rounded: bool, default=False
+                When set to ``True``, draw node boxes with rounded corners and use
+                Helvetica fonts instead of Times-Roman.
+        precision: int, default=3
+                Number of digits of precision for floating point in the values of
+                impurity, threshold and value attributes of each node.
+        ax: matplotlib axis, default=None
+                Axes to plot to. If None, use current axis. Any previous content
+                is cleared.
+        fontsize: int, default=None
+                Size of text font. If None, determined automatically to fit figure.
+    Returns:
+
+    """
+    check_is_fitted(causal_tree)
+
+    exporter = _MPLCTreeExporter(
+        causal_tree=causal_tree,
+        max_depth=max_depth,
+        feature_names=feature_names,
+        class_names=class_names,
+        label=label,
+        filled=filled,
+        impurity=impurity,
+        groups_count=groups_count,
+        treatment_groups=treatment_groups,
+        node_ids=node_ids,
+        proportion=proportion,
+        rounded=rounded,
+        precision=precision,
+        fontsize=fontsize,
+    )
+    exporter.export(causal_tree, ax=ax)