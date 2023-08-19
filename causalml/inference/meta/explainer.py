26f44232ab5bb985e343cd61e52e8d28dbc93f1e
Yifeng Wu <yifeng.wu@uber.com>
2023-08-18 17:55:35 -0700

    pass figsize

--- causalml/inference/meta/explainer.py
+++ causalml/inference/meta/explainer.py
@@ -200,7 +200,7 @@ class Explainer(object):
 
         return shap_dict
 
-    def plot_importance(self, importance_dict=None, title_prefix=""):
+    def plot_importance(self, importance_dict=None, title_prefix="", figsize=(12, 8)):
         """
         Calculates and plots feature importances for each treatment group, based on specified method in __init__.
         Skips the calculation part if importance_dict is given.
@@ -209,7 +209,7 @@ class Explainer(object):
             importance_dict = self.get_importance()
         for group, series in importance_dict.items():
             plt.figure()
-            series.sort_values().plot(kind="barh", figsize=(12, 8))
+            series.sort_values().plot(kind="barh", figsize=figsize)
             title = group
             if title_prefix != "":
                 title = "{} - {}".format(title_prefix, title)
