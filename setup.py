8ca1e588997968d998f6197661f4e1d28d769a50
Alexander Popkov <alr.popkov@gmail.com>
2023-10-01 23:00:13 +0300

    Keep line-by-line cython modules description in setup.py

--- setup.py
+++ setup.py
@@ -16,30 +16,17 @@ except ImportError:
     dist.Distribution().fetch_build_eggs(["numpy"])
     from numpy import get_include as np_get_include
 
+# fmt: off
 cython_modules = [
     ("causalml.inference.tree._tree._tree", "causalml/inference/tree/_tree/_tree.pyx"),
-    (
-        "causalml.inference.tree._tree._criterion",
-        "causalml/inference/tree/_tree/_criterion.pyx",
-    ),
-    (
-        "causalml.inference.tree._tree._splitter",
-        "causalml/inference/tree/_tree/_splitter.pyx",
-    ),
-    (
-        "causalml.inference.tree._tree._utils",
-        "causalml/inference/tree/_tree/_utils.pyx",
-    ),
-    (
-        "causalml.inference.tree.causal._criterion",
-        "causalml/inference/tree/causal/_criterion.pyx",
-    ),
-    (
-        "causalml.inference.tree.causal._builder",
-        "causalml/inference/tree/causal/_builder.pyx",
-    ),
+    ("causalml.inference.tree._tree._criterion", "causalml/inference/tree/_tree/_criterion.pyx"),
+    ("causalml.inference.tree._tree._splitter", "causalml/inference/tree/_tree/_splitter.pyx"),
+    ("causalml.inference.tree._tree._utils", "causalml/inference/tree/_tree/_utils.pyx"),
+    ("causalml.inference.tree.causal._criterion", "causalml/inference/tree/causal/_criterion.pyx"),
+    ("causalml.inference.tree.causal._builder", "causalml/inference/tree/causal/_builder.pyx"),
     ("causalml.inference.tree.uplift", "causalml/inference/tree/uplift.pyx"),
 ]
+# fmt: on
 
 extensions = [
     Extension(
