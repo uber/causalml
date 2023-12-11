2ee63b7e2dd4ab6cfe313d8e71280539d20f48fd
Roland Stevenson <rolandrmgservices@gmail.com>
2023-12-11 16:35:59 +0100

    minimal wrapper of MAQ #662

---
+++ causalml/metrics/multiqini.py
@@ -0,0 +1,16 @@
+from maq import MAQ, get_ipw_scores
+
+
+def get_mq_ipw_scores(Y, W, W_hat=None):
+    """Construct evaluation scores via inverse-propensity weighting.
+    See https://github.com/grf-labs/maq/blob/master/python-package/maq/maq.py
+    """
+    return get_ipw_scores(Y, W, W_hat)
+
+
+class MultiQini(MAQ):
+    """Fit a Multi-Armed Qini.
+    See https://github.com/grf-labs/maq/blob/master/python-package/maq/maq.py
+    """
+
+    pass
