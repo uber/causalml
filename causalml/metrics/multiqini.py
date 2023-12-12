8c07668e1fb033c1b7f8ee9c7643f8d506b754cb
Roland Stevenson <rolandrmgservices@gmail.com>
2023-12-12 10:31:32 +0100

    inherit maq docstrings

--- causalml/metrics/multiqini.py
+++ causalml/metrics/multiqini.py
@@ -1,17 +1,17 @@
 from maq import MAQ, get_ipw_scores
 
+#  See https://github.com/grf-labs/maq/blob/master/python-package/maq/maq.py
+
 
 def get_mq_ipw_scores(Y, W, W_hat=None):
-    """Construct evaluation scores via inverse-propensity weighting.
-    See https://github.com/grf-labs/maq/blob/master/python-package/maq/maq.py
-    """
-
     return get_ipw_scores(Y, W, W_hat)
 
 
-class MultiQini(MAQ):
-    """Fit a Multi-Armed Qini.
-    See https://github.com/grf-labs/maq/blob/master/python-package/maq/maq.py
-    """
+get_mq_ipw_scores.__doc__ = get_ipw_scores.__doc__
 
+
+class MultiQini(MAQ):
     pass
+
+
+MultiQini.__doc__ = MAQ.__doc__
