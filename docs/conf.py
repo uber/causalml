d12feb111164dfe2795d8c23d3b016b78d9c4cf7
Jeong-Yoon Lee <jeongyoon.lee1@gmail.com>
2024-04-19 19:18:39 -0700

    remove build for 32-bit systems that are failing

--- docs/conf.py
+++ docs/conf.py
@@ -133,7 +133,7 @@ html_theme = "sphinx_rtd_theme"
 html_theme_options = {"logo_only": False, "display_version": True}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
