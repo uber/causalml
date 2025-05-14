fix incorrect bootstrap mean calculation in meta-learners (#828)

causalml/inference/meta/drlearner.py
@@ -434,7 +434,7 @@ class BaseDRLearner(BaseLearner):
                 cate_b = self.bootstrap(
                     X, treatment, y, p, size=bootstrap_size, seed=seed
                 )
-                ate_bootstraps[:, n] = cate_b.mean()
+                ate_bootstraps[:, n] = cate_b.mean(axis=0)
 
             ate_lower = np.percentile(
                 ate_bootstraps, (self.ate_alpha / 2) * 100, axis=1

causalml/inference/meta/rlearner.py
@@ -324,7 +324,7 @@ class BaseRLearner(BaseLearner):
                 else:
                     p = self._format_p(p, self.t_groups)
                 cate_b = self.bootstrap(X, treatment, y, p, size=bootstrap_size)
-                ate_bootstraps[:, n] = cate_b.mean()
+                ate_bootstraps[:, n] = cate_b.mean(axis=0)
 
             ate_lower = np.percentile(
                 ate_bootstraps, (self.ate_alpha / 2) * 100, axis=1